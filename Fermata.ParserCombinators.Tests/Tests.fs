8c5858cd9ffa63f8a2f71d25648697f165b9bce4
taidalog <72747279+taidalog@users.noreply.github.com>
2024-05-25 14:27:51 +0900

    Wrote tests for `char'` parser. #3
    All tests are successful.

--- Fermata.ParserCombinators.Tests/Tests.fs
+++ Fermata.ParserCombinators.Tests/Tests.fs
@@ -2,7 +2,22 @@ module Tests
 
 open System
 open Xunit
+open Fermata.ParserCombinators.Parsers
 
 [<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+let ``char' 1`` () =
+    let expected = Ok('f', State("fsharp", 1))
+    let actual = char' 'f' (State("fsharp", 0))
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``char' 2`` () =
+    let expected = Ok('s', State("fsharp", 2))
+    let actual = char' 's' (State("fsharp", 1))
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``char' 3`` () =
+    let expected = Error("", State("fsharp", 0))
+    let actual = char' 'c' (State("fsharp", 0))
+    Assert.Equal(expected, actual)
